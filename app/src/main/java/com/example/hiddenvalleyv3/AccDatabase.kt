417cb99c410e4527b24dfa33475648a80d8cdeed
pohreng <56568717+pohreng@users.noreply.github.com>
2020-01-08 21:10:26 +0800

    123

---
+++ app/src/main/java/com/example/hiddenvalleyv3/AccDatabase.kt
@@ -0,0 +1,125 @@
+package com.example.hiddenvalleyv3
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+
+class AccDatabase (context: Context): SQLiteOpenHelper(context, dbname, factory, version){
+
+    override fun onCreate(p0: SQLiteDatabase?){
+        p0?.execSQL("create table info(id integer primary key autoincrement,"+
+                "username varchar(15), pass varchar(15),points integer)")
+        /*p0?.execSQL("create table newUser(username varchar(15) primary key autoincrement," +
+                "pass varchar(15));")
+        p0?.execSQL("create table info(username varchar(15)," +
+                "password varchar(15), points integer);")*/
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, oldVersion: Int, newVersion: Int){
+        p0?.execSQL("DROP TABLE IF EXISTS 'info'")
+        onCreate(p0)
+    }
+
+    fun insertUserData(username: String , pass: String){
+        val db: SQLiteDatabase =  writableDatabase
+        val info: ContentValues = ContentValues()
+
+        info.put("username", username)
+        info.put("pass", pass)
+        info.put("points", 0)
+
+        db.insert("info", null, info)
+        db.close()
+    }
+
+    fun verifyUsername(username: String): Boolean {
+        val db=  writableDatabase
+        val query= "select * from info where username = '$username'"
+        val cursor= db.rawQuery(query,null)
+        if ( cursor.count <= 0){
+            cursor.close()
+            return true
+        }
+        cursor.close()
+        return false
+    }
+
+    fun userPresent(username: String,pass: String): Boolean {
+        val db=  writableDatabase
+        val query= "select * from info where username = '$username' and pass = '$pass'"
+        val cursor= db.rawQuery(query,null)
+        if ( cursor.count <= 0){
+            cursor.close()
+            return false
+        }
+        cursor.close()
+        return true
+    }
+
+    fun retrieveData(username: String) : MutableList<User_details>{
+        var list : MutableList<User_details> = ArrayList()
+
+        val db = this.readableDatabase
+        val query = "Select * from info where username = '$username' "
+        val result = db.rawQuery(query,null)
+        if(result.moveToFirst()){
+            do{
+                var user = User_details()
+                user.username = result.getString(result.getColumnIndex("username"))
+                user.password = result.getString(result.getColumnIndex("pass"))
+                user.points = result.getString(result.getColumnIndex("points")).toInt()
+                list.add(user)
+            }while (result.moveToNext())
+        }
+        result.close()
+        db.close()
+        return list
+    }
+    fun increasePoint(username: String , pass: String,points: Int){
+        val db: SQLiteDatabase =  writableDatabase
+        val info: ContentValues = ContentValues()
+        val query = "Select * from info where username = '$username' "
+        val result = db.rawQuery(query,null)
+        if(result.count<=0){
+            val totalpoint = points + result.getString(result.getColumnIndex("points")).toInt()
+
+            //val query1 = "update info set values points = "+ totalpoint.toString().toInt() +" where username = '$username'"
+            info.put("points", totalpoint.toString().toInt())
+
+            db.insert("info", null, info)
+            db.close()
+
+            //db.rawQuery(query1,null)
+            //db.close()
+        }
+    }
+    fun retrievePoints(username: String) : MutableList<User_details>{
+        var list : MutableList<User_details> = ArrayList()
+
+        val db = this.readableDatabase
+        val query = "Select * from info where username = '$username' "
+        val result = db.rawQuery(query,null)
+        if(result.moveToFirst()){
+            do{
+                var user = User_details()
+                user.username = result.getString(result.getColumnIndex("username"))
+                user.password = result.getString(result.getColumnIndex("pass"))
+                user.points = result.getString(result.getColumnIndex("points")).toInt()
+                list.add(user)
+            }while (result.moveToNext())
+        }
+        result.close()
+        db.close()
+        return list
+    }    companion object{
+
+        internal val dbname = "userDB"
+        internal val factory = null
+        internal val version = 1
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/hiddenvalleyv3/MainActivity.kt
@@ -0,0 +1,539 @@
+package com.example.hiddenvalleyv3
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
+import android.content.res.Configuration
+import android.content.res.Resources
+import android.database.sqlite.SQLiteDatabase
+import android.os.Bundle
+import android.text.AlteredCharSequence
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.after_login.*
+import kotlinx.android.synthetic.main.cal_game.*
+import kotlinx.android.synthetic.main.game.*
+import kotlinx.android.synthetic.main.login.*
+import kotlinx.android.synthetic.main.number_game.*
+import kotlinx.android.synthetic.main.user_profile.*
+import kotlinx.android.synthetic.main.user_registration.*
+import kotlinx.android.synthetic.main.user_registration.pass
+import kotlinx.android.synthetic.main.user_registration.username
+import kotlinx.android.synthetic.main.user_setting.*
+import java.util.*
+
+class  MainActivity : AppCompatActivity() {
+
+    lateinit var handler: AccDatabase
+    lateinit var diceImage1 : ImageView
+    lateinit var diceImage2 : ImageView
+    lateinit var starImage : ImageView
+
+    //var manager = supportFragmentManager
+    //val animationIn = AnimationUtils.loadAnimation(this,R.anim.zoom_in)
+    //val animationOut = AnimationUtils.loadAnimation(this,R.anim.zoom_out)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        loadLocate()
+        setContentView(R.layout.activity_main)
+
+        handler = AccDatabase(this)
+
+        val actionBar = supportActionBar
+        actionBar!!.title = resources.getString(R.string.app_name)
+
+        showHome()
+
+        registration_button.setOnClickListener{
+            showUserReg()
+        }
+
+        login_button.setOnClickListener{
+            showLogin()
+        }
+
+        sign_button.setOnClickListener {
+            if(username.text.toString().isNotEmpty()){
+                if (handler.verifyUsername(username.text.toString())) {
+                    if (pass.text.toString().isNotEmpty() && pass.text.toString() == password.text.toString()) {
+                        handler.insertUserData(username.text.toString(), pass.text.toString())
+                        showHome()
+                    } else {
+                        Toast.makeText(this, "Incorrect Second Password", Toast.LENGTH_SHORT).show()
+                        showUserReg()
+                    }
+                } else
+                    Toast.makeText(this, "Username Been Used", Toast.LENGTH_SHORT).show()
+            } else
+                Toast.makeText(this, "Username cannot be empty", Toast.LENGTH_SHORT).show()
+        }
+
+        login.setOnClickListener{
+            login()
+        }
+    }
+
+    private fun login(){
+        if(handler.userPresent(login_username.text.toString(),login_pass.text.toString())) {
+            Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show()
+
+            var data = handler.retrieveData(login_username.text.toString())
+
+            afterLoginPages()
+
+            afterUsername.text = ""
+            for(i in 0..(data.size-1)){
+                afterUsername.append(data.get(i).username)
+            }
+            val clickerProfile = findViewById<ImageView>(R.id.current_profile)
+            val clickerLetter = findViewById<ImageView>(R.id.letterIMG)
+            val clickerNumber = findViewById<ImageView>(R.id.numberIMG)
+            val clickerMatch = findViewById<ImageView>(R.id.matchIMG)
+            val clickerCal = findViewById<ImageView>(R.id.calculateIMG)
+
+            clickerProfile.setOnClickListener{
+                profile()
+            }
+            clickerLetter.setOnClickListener{
+                showLetGame()
+            }
+            clickerNumber.setOnClickListener{
+                showNumGame()
+                starImage = findViewById(R.id.star_img)
+                starImage.setImageResource(R.drawable.star0)
+                num_btn.setOnClickListener{
+                    rollStar()
+                }
+            }
+            clickerMatch.setOnClickListener{
+                showMatchGame()
+            }
+            clickerCal.setOnClickListener{
+                showCalGame()
+                diceImage1 = findViewById(R.id.dice_img1)
+                diceImage2 = findViewById(R.id.dice_img2)
+
+                diceImage1.setImageResource(R.drawable.dice_1)
+                diceImage2.setImageResource(R.drawable.dice_1)
+
+                roll_button.setOnClickListener{
+                    rollDice()
+                }
+            }
+        }else {
+            Toast.makeText(this, "Username Or Password Incorrect", Toast.LENGTH_SHORT).show()
+            showLogin()
+        }
+    }
+
+
+    private fun rollDice(){
+
+        val randomInt1 = Random().nextInt(6)+1
+        val randomInt2 = Random().nextInt(6)+1
+
+        val dice1 = when (randomInt1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage1.setImageResource(dice1)
+
+        val dice2 = when (randomInt2) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage2.setImageResource(dice2)
+        if(cal_answer.text.toString().isNotEmpty()) {
+            answer_calculation.setOnClickListener {
+                countTotal(randomInt1, randomInt2)
+            }
+        }else{
+            Toast.makeText(this, "Please Enter Answer", Toast.LENGTH_SHORT).show()
+        }
+        backOrNext.setOnClickListener{
+            afterLoginPages()
+        }
+    }
+    private fun countTotal(num1 : Int,num2 : Int){
+        val ans = cal_answer.text.toString().toInt()
+        val streak = cal_game_streak.text.toString().toInt() + 1
+        val total = num1 + num2
+        if(ans == total) {
+            Toast.makeText(this, "Answer Correct", Toast.LENGTH_SHORT).show()
+            val plusPoints = 10 + streak
+            handler.increasePoint(login_username.text.toString(),login_pass.text.toString(),plusPoints)
+            val newStreak = streak + 0
+            cal_game_streak.text = newStreak.toString()
+            rollDice()
+        }
+        else {
+            val newStreak = 0
+            cal_game_streak.text = newStreak.toString()
+            Toast.makeText(this, "Wrong Answer", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun rollStar(){
+        val randomInt1 = Random().nextInt(8)+1
+
+        val star = when (randomInt1) {
+            1 -> R.drawable.star1
+            2 -> R.drawable.star2
+            3 -> R.drawable.star3
+            4 -> R.drawable.star4
+            5 -> R.drawable.star5
+            6 -> R.drawable.star6
+            7 -> R.drawable.star7
+            else -> R.drawable.star8
+        }
+        starImage.setImageResource(star)
+
+        if(num_answer.text.toString().isNotEmpty()) {
+            ans_number.setOnClickListener {
+                countTotalStar(randomInt1)
+            }
+        }else{
+            Toast.makeText(this, "Please Enter Answer", Toast.LENGTH_SHORT).show()
+        }
+        backOrNext_number.setOnClickListener{
+            afterLoginPages()
+        }
+    }
+    private fun countTotalStar(num1 : Int){
+        val ans = num_answer.text.toString().toInt()
+        val streak = num_game_streak.text.toString().toInt() + 1
+        if(ans == num1) {
+            Toast.makeText(this, "Answer Correct", Toast.LENGTH_SHORT).show()
+            val plusPoints = 10 + streak
+            handler.increasePoint(login_username.text.toString(),login_pass.text.toString(),plusPoints)
+            val newStreak = streak + 0
+            num_game_streak.text = newStreak.toString()
+            rollStar()
+        }
+        else {
+            val newStreak = 0
+            num_game_streak.text = newStreak.toString()
+            Toast.makeText(this, "Wrong Answer", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    /*private fun loopLogin(){
+    var data = handler.retrieveData(login_username.text.toString())
+
+    setContentView(R.layout.after_login)
+
+    afterUsername.text = ""
+    for(i in 0..(data.size-1)){
+        afterUsername.append(data.get(i).username)
+    }
+
+    val clickerLetter = findViewById<ImageView>(R.id.letterIMG)
+    val clickerNumber = findViewById<ImageView>(R.id.numberIMG)
+    val clickerMatch = findViewById<ImageView>(R.id.matchIMG)
+    val clickerCal = findViewById<ImageView>(R.id.calculateIMG)
+
+    clickerLetter.setOnClickListener{
+        setContentView(R.layout.game)
+        showLetterGame()
+        back_home.setOnClickListener {
+            login()
+        }
+    }
+    clickerNumber.setOnClickListener{
+        setContentView(R.layout.game)
+        showNumberGame()
+        back_home.setOnClickListener {
+            login()
+        }
+    }
+    clickerMatch.setOnClickListener{
+        setContentView(R.layout.game)
+        showMatchGame()
+        back_home.setOnClickListener {
+            login()
+        }
+    }
+    clickerCal.setOnClickListener{
+        setContentView(R.layout.game)
+        showCalGame()
+        back_home.setOnClickListener {
+            login()
+        }
+    }
+}*/
+    /*private fun showCalGame(){
+        val transaction = manager.beginTransaction()
+        val fragment = Cal_game_fragment()
+        transaction.replace(R.id.fragment_holder,fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }*/
+    /*private fun showLetterGame(){
+        val transaction = manager.beginTransaction()
+        val fragment = Letter_game_fragment()
+        transaction.replace(R.id.fragment_holder,fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }*/
+    /*private fun showMatchGame(){
+        val transaction = manager.beginTransaction()
+        val fragment = Match_game_fragment()
+        transaction.replace(R.id.fragment_holder,fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }*/
+    /* private fun showNumberGame(){
+        val transaction = manager.beginTransaction()
+        val fragment = Number_game_fragment()
+        transaction.replace(R.id.fragment_holder,fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }*/
+    /*private fun changeImg(){
+
+        val randomInt = Random().nextInt(8)+1
+
+        val newProfilePic = when (randomInt){
+            1 -> R.drawable.profilepic1
+            2 -> R.drawable.profilepic2
+            3 -> R.drawable.profilepic3
+            4 -> R.drawable.profilepic4
+            5 -> R.drawable.profilepic5
+            6 -> R.drawable.profilepic6
+            7 -> R.drawable.profilepic7
+            else -> R.drawable.profilepic8
+        }
+
+        profilePic.setImageResource(newProfilePic)
+    }*/
+
+    private fun changeLang(){
+        val listItems = arrayOf("华语","Malay")
+
+        val mBuilder = AlertDialog.Builder(this@MainActivity)
+        mBuilder.setTitle("Choose Language")
+        mBuilder.setSingleChoiceItems(listItems,-1){dialog, which ->
+            if(which == 0){
+                setLocate("zh")
+                Toast.makeText(this, "LOG IN AGAIN", Toast.LENGTH_SHORT).show()
+                recreate()
+            }
+            else if(which == 1){
+                setLocate("ms")
+                Toast.makeText(this, "LOG IN AGAIN", Toast.LENGTH_SHORT).show()
+                recreate()
+            }
+            dialog.dismiss()
+        }
+        val mDialog = mBuilder.create()
+        mDialog.show()
+    }
+    private fun setLocate(Lang : String){
+        val locale = Locale(Lang)
+        Locale.setDefault(locale)
+        val config = Configuration()
+
+        config.locale = locale
+        baseContext.resources.updateConfiguration(config,baseContext.resources.displayMetrics)
+
+        val editor = getSharedPreferences("Settings", Context.MODE_PRIVATE).edit()
+        editor.putString("My_Lang",Lang)
+        editor.apply()
+    }
+    private fun loadLocate(){
+        val sharedPreferences = getSharedPreferences("Settings", Activity.MODE_PRIVATE)
+        val lang = sharedPreferences.getString("My_Lang","")
+        setLocate(lang)
+    }
+    private fun profile(){
+        var data1 = handler.retrieveData(login_username.text.toString())
+        showProfile()
+        current_username1.text = ""
+        for(i in 0..(data1.size-1)){
+            current_username1.append(data1.get(i).username)
+        }
+
+
+        var data = handler.retrievePoints(login_username.text.toString())
+        current_points.text = ""
+        for(i in 0..(data.size-1)){
+            current_points.append(data.get(i).points.toString())
+        }
+
+        val clickerPic = findViewById<ImageView>(R.id.current_profile12)
+        clickerPic.setOnClickListener{
+            afterLoginPages()
+        }
+
+        btn_point.setOnClickListener{
+            profile()
+        }
+        btn_setting.setOnClickListener{
+            setting()
+        }
+    }
+    private fun setting(){
+        var data1 = handler.retrieveData(login_username.text.toString())
+        showUserSetting()
+        current_username2.text = ""
+        for(i in 0..(data1.size-1)){
+            current_username2.append(data1.get(i).username)
+        }
+        val clickerPic = findViewById<ImageView>(R.id.current_profile123)
+        clickerPic.setOnClickListener{
+            afterLoginPages()
+        }
+        val btnChg = findViewById<Button>(R.id.change_language)
+
+        btnChg.setOnClickListener{
+            changeLang()
+        }
+        sign_out.setOnClickListener{
+            showHome()
+        }
+        btn_point1.setOnClickListener{
+            profile()
+        }
+        btn_setting1.setOnClickListener{
+            setting()
+        }
+    }
+    private fun showUserReg(){
+        let_game.visibility=View.GONE
+        match_game.visibility=View.GONE
+        num_game.visibility=View.GONE
+        cal_game.visibility=View.GONE
+        registration_layout.visibility=View.VISIBLE
+        login_layout.visibility=View.GONE
+        main123.visibility=View.GONE
+        afterLogin_layout.visibility=View.GONE
+        user_profile.visibility=View.GONE
+        user_setting.visibility=View.GONE
+    }
+    private fun showLogin(){
+        let_game.visibility=View.GONE
+        match_game.visibility=View.GONE
+        num_game.visibility=View.GONE
+        cal_game.visibility=View.GONE
+        registration_layout.visibility=View.GONE
+        login_layout.visibility=View.VISIBLE
+        main123.visibility=View.GONE
+        afterLogin_layout.visibility=View.GONE
+        user_profile.visibility=View.GONE
+        user_setting.visibility=View.GONE
+    }
+    private fun showHome(){
+        let_game.visibility=View.GONE
+        match_game.visibility=View.GONE
+        num_game.visibility=View.GONE
+        cal_game.visibility=View.GONE
+        registration_layout.visibility=View.GONE
+        login_layout.visibility=View.GONE
+        main123.visibility=View.VISIBLE
+        afterLogin_layout.visibility=View.GONE
+        user_profile.visibility=View.GONE
+        user_setting.visibility=View.GONE
+    }
+    private fun afterLoginPages(){
+        let_game.visibility=View.GONE
+        match_game.visibility=View.GONE
+        num_game.visibility=View.GONE
+        cal_game.visibility=View.GONE
+        registration_layout.visibility=View.GONE
+        login_layout.visibility=View.GONE
+        main123.visibility=View.GONE
+        afterLogin_layout.visibility=View.VISIBLE
+        user_profile.visibility=View.GONE
+        user_setting.visibility=View.GONE
+    }
+    private fun showProfile(){
+        let_game.visibility=View.GONE
+        match_game.visibility=View.GONE
+        num_game.visibility=View.GONE
+        cal_game.visibility=View.GONE
+        registration_layout.visibility=View.GONE
+        login_layout.visibility=View.GONE
+        main123.visibility=View.GONE
+        afterLogin_layout.visibility=View.GONE
+        user_profile.visibility=View.VISIBLE
+        user_setting.visibility=View.GONE
+    }
+    private fun showUserSetting(){
+        let_game.visibility=View.GONE
+        match_game.visibility=View.GONE
+        num_game.visibility=View.GONE
+        cal_game.visibility=View.GONE
+        registration_layout.visibility=View.GONE
+        login_layout.visibility=View.GONE
+        main123.visibility=View.GONE
+        afterLogin_layout.visibility=View.GONE
+        user_profile.visibility=View.GONE
+        user_setting.visibility=View.VISIBLE
+    }
+
+    private fun showNumGame(){
+        let_game.visibility=View.GONE
+        match_game.visibility=View.GONE
+        num_game.visibility=View.VISIBLE
+        cal_game.visibility=View.GONE
+        registration_layout.visibility=View.GONE
+        login_layout.visibility=View.GONE
+        main123.visibility=View.GONE
+        afterLogin_layout.visibility=View.GONE
+        user_profile.visibility=View.GONE
+        user_setting.visibility=View.GONE
+    }
+    private fun showLetGame(){
+        let_game.visibility=View.VISIBLE
+        match_game.visibility=View.GONE
+        num_game.visibility=View.GONE
+        cal_game.visibility=View.GONE
+        registration_layout.visibility=View.GONE
+        login_layout.visibility=View.GONE
+        main123.visibility=View.GONE
+        afterLogin_layout.visibility=View.GONE
+        user_profile.visibility=View.GONE
+        user_setting.visibility=View.GONE
+    }
+    private fun showMatchGame(){
+        let_game.visibility=View.GONE
+        match_game.visibility=View.VISIBLE
+        num_game.visibility=View.GONE
+        cal_game.visibility=View.GONE
+        match_game.visibility=View.GONE
+        registration_layout.visibility=View.GONE
+        login_layout.visibility=View.GONE
+        main123.visibility=View.GONE
+        afterLogin_layout.visibility=View.GONE
+        user_profile.visibility=View.GONE
+        user_setting.visibility=View.GONE
+        user_setting.visibility=View.GONE
+    }
+    private fun showCalGame(){
+        let_game.visibility=View.GONE
+        match_game.visibility=View.GONE
+        num_game.visibility=View.GONE
+        cal_game.visibility=View.VISIBLE
+        registration_layout.visibility=View.GONE
+        login_layout.visibility=View.GONE
+        main123.visibility=View.GONE
+        afterLogin_layout.visibility=View.GONE
+        user_profile.visibility=View.GONE
+        user_setting.visibility=View.GONE
+    }
+}
